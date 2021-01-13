c4244c0e6d1477f624a56be8f2432964bc49c478
moriero <moriero@hotmail.it>
2021-01-13 14:30:18 +0100

    Update BookList.vue

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,43 +1,41 @@
-
-
 <template>
-<div>
+  <div>
     <h1>{{ title }}</h1>
     <ul>
-      
-<li v-for="book in books" :key="book.title">{{book.title}}:{{book.author}}</li>
-    
+      <li v-for="book in books" :key="book.title">
+        {{ book.title }}:{{ book.author }}
+      </li>
     </ul>
-</div>
+  </div>
 </template>
-    
+
 <script>
 export default {
-    name : BookList,
-    data : {
-        title : "All Books",
-        books: [
-              {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-    {title: 'American Gods', author: 'Neil Gaiman'},
-    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
-        ]
-    }
-}
+  name: BookList,
+  data: {
+    title: "All Books",
+    books: [
+      { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+      { title: "American Gods", author: "Neil Gaiman" },
+      { title: "Amusing Ourselves to Death", author: "Neil Postman" },
+    ],
+  },
+};
 </script>
 
-
 <style>
- h1, h2 {
-     font-weight : normal
- }
+h1,
+h2 {
+  font-weight: normal;
+}
 
- ul {
-     list-style-type: none;
-     padding: 0
- }
+ul {
+  list-style-type: none;
+  padding: 0;
+}
 
- li {
-     display: block;
-     margin: 0 10px;
- }
+li {
+  display: block;
+  margin: 0 10px;
+}
 </style>
